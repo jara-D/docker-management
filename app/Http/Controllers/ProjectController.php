21f7c9ae3203a66a9e5f548c0271fec82ef452a9
Jara Doornbos <jaradoornbos1@gmail.com>
2026-04-17 09:44:27 +0200

    whoops forgot to change something in an if statement

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -92,14 +92,12 @@ class ProjectController extends Controller
             $response = $this->go->stopContainer($container->container_id);
             $decoded = json_decode(json_encode($response));
 
-            if (!isset($decoded->result->state) || $decoded->result->state !== 'started') {
+            if (!isset($decoded->result->state) || $decoded->result->state !== 'exited') {
                 $failures[] = $container->container_id;
             }
         }
 
-        Log::info(json_encode($failures));
-
-        $dockerData = $this->go->listContainers(); // or inspect each container
+        $dockerData = $this->go->listContainers();
 
         app(ContainerSyncService::class)->projectSync(
             $project->id,
